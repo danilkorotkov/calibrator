a36726f4200f29a9a09f12ceea901113467c715b
danilkorotkov <spballiance@gmail.com>
2017-04-20 12:29:24 +0300

    ui

--- calibrator.py
+++ calibrator.py
@@ -31,22 +31,120 @@ class Calibrator ( QtGui.QMainWindow, Ui_Calibrator ):
     'Channel6':[3.5708,5.3255,319.73,249.65],
     'Counter1':0,
     'Counter2':0}
+    A3=0
+    A2=0
+    A1=0
+    A0=0
+    Volts=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     
     def __init__ ( self, parent = None ):
         super(Calibrator, self).__init__(parent)
         Ui_Calibrator.__init__(self)
         self.setupUi( self )
         self.move(463, 345)
-        self.pushButton.pressed.connect(self.test1)
+        self.setWindowModality(QtCore.Qt.WindowModal)
+        #self.pushButton.pressed.connect(self.test1)
         self.Exit.pressed.connect(self.close)
-
+        self.pBtn_Channel_1.pressed.connect(self.Btn1)
+        self.pBtn_Channel_2.pressed.connect(self.Btn2)
+        self.pBtn_Channel_3.pressed.connect(self.Btn3)
+        self.pBtn_Channel_4.pressed.connect(self.Btn4)
+        self.pBtn_Channel_5.pressed.connect(self.Btn5)
+        self.pBtn_Channel_6.pressed.connect(self.Btn6)
+        self.pushButton_2.pressed.connect(self.Get_Volts)
+        self.pushButton_3.pressed.connect(self.Calc)
+        #self.test1()
+        self.Show_Volts()
+        
     def __del__ ( self ):
         self.ui = None
 
-    def test1(self):
+    def init(self):
+        pass
+#        for p in range(1,7):
+#            for i in range (4):
+#                getattr(self, 'listWidget_'+ str(p)).addItem(str(self.a['Channel'+str(p)][i]))
+
+    def Calc(self):
+        if self.pBtn_Channel_1.isChecked():
+            self.test1(0)
+        elif self.pBtn_Channel_2.isChecked():
+            self.test1(1)
+        elif self.pBtn_Channel_3.isChecked():
+            self.test1(2)
+        elif self.pBtn_Channel_4.isChecked():
+            self.test1(3)
+        elif self.pBtn_Channel_5.isChecked():
+            self.test1(4)
+        elif self.pBtn_Channel_6.isChecked():
+            self.test1(5)
+        else:
+            pass
+
+
+    def Show_Volts(self):
+        self.lineEdit_1.setText(str(self.Volts[0][0]))
+        self.lineEdit_2.setText(str(self.Volts[0][1]))
+        self.lineEdit_3.setText(str(self.Volts[0][2]))
+        self.lineEdit_4.setText(str(self.Volts[0][3]))
+        self.lineEdit_5.setText(str(self.Volts[1][0]))
+        self.lineEdit_6.setText(str(self.Volts[1][1]))
+        self.lineEdit_7.setText(str(self.Volts[1][2]))
+        self.lineEdit_8.setText(str(self.Volts[1][3]))
+        self.lineEdit_9.setText(str(self.Volts[2][0]))
+        self.lineEdit_10.setText(str(self.Volts[2][1]))
+        self.lineEdit_11.setText(str(self.Volts[2][2]))
+        self.lineEdit_12.setText(str(self.Volts[2][3]))
+        self.lineEdit_13.setText(str(self.Volts[3][0]))
+        self.lineEdit_14.setText(str(self.Volts[3][1]))
+        self.lineEdit_15.setText(str(self.Volts[3][2]))
+        self.lineEdit_16.setText(str(self.Volts[3][3]))
+        self.lineEdit_17.setText(str(self.Volts[4][0]))
+        self.lineEdit_18.setText(str(self.Volts[4][1]))
+        self.lineEdit_19.setText(str(self.Volts[4][2]))
+        self.lineEdit_20.setText(str(self.Volts[4][3]))
+        self.lineEdit_21.setText(str(self.Volts[5][0]))
+        self.lineEdit_22.setText(str(self.Volts[5][1]))
+        self.lineEdit_23.setText(str(self.Volts[5][2]))
+        self.lineEdit_24.setText(str(self.Volts[5][3]))
+
+    def Get_Volts(self):
+        self.Volts[0][0]=float(self.lineEdit_1.text())
+        self.Volts[0][1]=float(self.lineEdit_2.text())
+        self.Volts[0][2]=float(self.lineEdit_3.text())
+        self.Volts[0][3]=float(self.lineEdit_4.text())
+        self.Volts[1][0]=float(self.lineEdit_5.text())
+        self.Volts[1][1]=float(self.lineEdit_6.text())
+        self.Volts[1][2]=float(self.lineEdit_7.text())
+        self.Volts[1][3]=float(self.lineEdit_8.text())
+        self.Volts[2][0]=float(self.lineEdit_9.text())
+        self.Volts[2][1]=float(self.lineEdit_10.text())
+        self.Volts[2][2]=float(self.lineEdit_11.text())
+        self.Volts[2][3]=float(self.lineEdit_12.text())
+        self.Volts[3][0]=float(self.lineEdit_13.text())
+        self.Volts[3][1]=float(self.lineEdit_14.text())
+        self.Volts[3][2]=float(self.lineEdit_15.text())
+        self.Volts[3][3]=float(self.lineEdit_16.text())
+        self.Volts[4][0]=float(self.lineEdit_17.text())
+        self.Volts[4][1]=float(self.lineEdit_18.text())
+        self.Volts[4][2]=float(self.lineEdit_19.text())
+        self.Volts[4][3]=float(self.lineEdit_20.text())
+        self.Volts[5][0]=float(self.lineEdit_21.text())
+        self.Volts[5][1]=float(self.lineEdit_22.text())
+        self.Volts[5][2]=float(self.lineEdit_23.text())
+        self.Volts[5][3]=float(self.lineEdit_24.text())
+        
+        self.Show_Volts()
+        
+    def test1(self, x):
         self.textEdit.setText("")
         xy=[[0.766,0.0],[1.2775,174.95],[1.5347,266.5],[2.3016,558,001]]
         A=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+        
+        xy[0][0]=self.Volts[x][0]
+        xy[1][0]=self.Volts[x][1]
+        xy[2][0]=self.Volts[x][2]
+        xy[3][0]=self.Volts[x][3]
         
         a6=0
         a5=0
@@ -108,8 +206,8 @@ class Calibrator ( QtGui.QMainWindow, Ui_Calibrator ):
             #print B3
             #print A
             detB3=numpy.linalg.det(B3)
-            A3=detB3/detA
-            print A3
+            self.A3=detB3/detA
+            #print A3
             B3[0][0]=a6
             B3[1][0]=a5
             B3[2][0]=a4
@@ -120,8 +218,8 @@ class Calibrator ( QtGui.QMainWindow, Ui_Calibrator ):
             B2[2][1]=b1
             B2[3][1]=b0
             detB2=numpy.linalg.det(B2)
-            A2=detB2/detA
-            print A2
+            self.A2=detB2/detA
+            #print A2
             B2[0][1]=a5
             B2[1][1]=a4
             B2[2][1]=a3
@@ -132,8 +230,8 @@ class Calibrator ( QtGui.QMainWindow, Ui_Calibrator ):
             B1[2][2]=b1
             B1[3][2]=b0
             detB1=numpy.linalg.det(B1)
-            A1=detB1/detA
-            print A1
+            self.A1=detB1/detA
+            #print A1
             B1[0][2]=a4
             B1[1][2]=a3
             B1[2][2]=a2
@@ -144,14 +242,51 @@ class Calibrator ( QtGui.QMainWindow, Ui_Calibrator ):
             B0[2][3]=b1
             B0[3][3]=b0
             detB0=numpy.linalg.det(B0)
-            A0=detB0/detA
-            print A0
+            self.A0=detB0/detA
+            #print A0
         
-        self.textEdit.setText(
-                "%.4fx" % A3+'\xB3'+self.sign(A2) + \
-                "%.4fx" % abs(A2)+'\xB2'+self.sign(A1) + \
-                "%.4fx" % abs(A1)+self.sign(A0) + \
-                "%.4f" % abs(A0))
+            if x==0:
+                self.a['Channel1'][0]=self.A3
+                self.a['Channel1'][1]=self.A2
+                self.a['Channel1'][2]=self.A1
+                self.a['Channel1'][3]=abs(self.A0)
+            elif x==1:    
+                self.a['Channel2'][0]=self.A3
+                self.a['Channel2'][1]=self.A2
+                self.a['Channel2'][2]=self.A1
+                self.a['Channel2'][3]=abs(self.A0)
+            elif x==2:
+                self.a['Channel3'][0]=self.A3
+                self.a['Channel3'][1]=self.A2
+                self.a['Channel3'][2]=self.A1
+                self.a['Channel3'][3]=abs(self.A0)
+            elif x==3:
+                self.a['Channel4'][0]=self.A3
+                self.a['Channel4'][1]=self.A2
+                self.a['Channel4'][2]=self.A1
+                self.a['Channel4'][3]=abs(self.A0)
+            elif x==4:
+                self.a['Channel5'][0]=self.A3
+                self.a['Channel5'][1]=self.A2
+                self.a['Channel5'][2]=self.A1
+                self.a['Channel5'][3]=abs(self.A0)
+            elif x==5:
+                self.a['Channel6'][0]=self.A3
+                self.a['Channel6'][1]=self.A2
+                self.a['Channel6'][2]=self.A1
+                self.a['Channel6'][3]=abs(self.A0)
+            else:
+                pass
+                
+        
+            self.textEdit.setText(
+                "%.4fx" % self.A3+'\xB3'+self.sign(self.A2) + \
+                "%.4fx" % abs(self.A2)+'\xB2'+self.sign(self.A1) + \
+                "%.4fx" % abs(self.A1)+self.sign(self.A0) + \
+                "%.4f" % abs(self.A0))
+        else:
+            self.textEdit.setText('detA=0')
+        self.textEdit.setAlignment(Qt.AlignCenter)
 
     def sign(self, tempor):
         if tempor>=0:
@@ -159,3 +294,23 @@ class Calibrator ( QtGui.QMainWindow, Ui_Calibrator ):
         else:
             out='-'
         return out
+        
+    def Btn1(self):
+        pass
+        
+    def Btn2(self):
+        pass
+        
+    def Btn3(self):
+        pass
+        
+    def Btn4(self):
+        pass
+        
+    def Btn5(self):
+        pass
+        
+    def Btn6(self):
+        pass
+        
+        
